8aa15256ea497f948a10b2861b19966bdb774fdf
今朝 <chen1990wb@163.com>
2020-07-19 23:25:13 +0800

    no message

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ module.exports = function () {
   //保存获取token的任务
   this.bindHook('mock_after', async function (context) {
     yapi.commons.log('mock_after,' + context);
-    const { ctx, mockJson } = context;
+    let { ctx, mockJson } = context;
     let result = await axios.get('https://apis.imooc.com?icode=' + ctx.header.icode);
     if (result.code !== 1000) {
-      mockJson = {
-        status: -1000,
-        msg: '请输入您的慕课网icode',
-      };
+      mockJson.status = -100;
+      mockJson.msg = '请在请求头中添加本门课程的icode';
+      mockJson.data = null;
     }
   });
 };
